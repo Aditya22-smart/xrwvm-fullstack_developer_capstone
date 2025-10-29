6f8fb1e34c061faf6a015bb0f8dc3946be8ee3ea
Aditya22-smart <adityalakhotia21@gmail.com>
2025-10-29 16:21:05 -0400

    Final edited

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -51,4 +51,32 @@ def post_review(data_dict):
         print(response.json())
         return response.json()
     except:
-        print("Network exception occurred")
+        print("Network exception occurred")apiVersion: apps/v1
+kind: Deployment
+metadata:
+  labels:
+    run: dealership
+  name: dealership
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      run: dealership
+  strategy:
+    rollingUpdate:
+      maxSurge: 25%
+      maxUnavailable: 25%
+    type: RollingUpdate
+  template:
+    metadata:
+      labels:
+        run: dealership
+    spec:
+      containers:
+      - image: us.icr.io/sn-labs-adityalakhot/dealership:latest
+        imagePullPolicy: Always
+        name: dealership
+        ports:
+        - containerPort: 8000
+          protocol: TCP
+      restartPolicy: Always
